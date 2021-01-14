a33e47e23c30ecce8b001d8e1856b314b06a3aff
Alcol <yuhang3661175@qq.com>
2021-01-14 23:03:16 +0800

    first commit

--- src/components/CommonCard/index.vue
+++ src/components/CommonCard/index.vue
@@ -1,14 +1,43 @@
 <template>
   <div class='common-card'>
-    <div class='title'></div>
-    <div class='value'></div>
-    <div class='chart'></div>
-    <div class='line'></div>
-    <div class='total'></div>
+    <div class='title'>{{title}}</div>
+    <div class='value'>{{value}}</div>
+    <div class='chart'>
+      <slot></slot>
+    </div>
+    <div class='line' />
+    <div class='total'>
+      <slot name='footer'></slot>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    title: String,
+    value: String
+  }
+}
 
 </script>
+
+<style lang="stylus">
+.title 
+  font-size 12px
+  color #999
+.value
+  font-size 25px
+  colot #000
+  margin-top 5px
+  letter-spacing 1px
+.chart 
+  height 50px
+  background yellow
+.line
+  margin 10px 0
+  border-top 1px solid #eee
+.total
+  color #666
+  font-size 20px      
+</style>
--- src/components/TotalSales/index.vue
+++ src/components/TotalSales/index.vue
@@ -1,9 +1,66 @@
 <template>
-  <div>topView</div>
+  <common-card title="累计销售额" value="￥ 67,541,154">
+    <template>
+      <div class='compare-wrapper'>
+        <div class="compare">
+        <span>日同比</span>
+        <span class="emphasis">7.33%</span>
+        <div class="increase"></div>
+      </div>
+      <div class="compare">
+        <span>月同比</span>
+        <span class="emphasis">7.33%</span>
+        <div class="decrease"></div>
+      </div>
+      </div>
+    </template>
+    <template v-slot:footer>
+      <span>昨日销售额</span>
+      <span class="emphasis">30,000,000</span>
+    </template>
+  </common-card>
 </template>
 
 <script>
+import commonCardMixin from '@/mixins/commonCardMixin'
 export default {
-  name: 'app'
+  mixins: [commonCardMixin]
 }
 </script>
+
+<style lang="stylus" scope>
+.compare-wrapper
+  height 100%
+  display flex
+  flex-direction column
+  justify-content center
+  .compare
+    font-size 12px
+    margin-top 3px
+    background yellow
+    color #666
+    display flex
+    align-items center
+    
+</style>
+
+<style lang="stylus">
+.emphasis
+  margin-left 5px
+  color #333
+  font-weight 700
+.increase 
+      width 0
+      height 0
+      border-width 4px
+      border-color transparent transparent red transparent
+      border-style solid
+      margin 0 0 3px 5px
+.decrease 
+  width 0
+  height 0
+  border-width 4px
+  border-color green transparent transparent transparent
+  border-style solid
+  margin 3 0 0 5px
+</style>
